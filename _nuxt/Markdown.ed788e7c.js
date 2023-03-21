17f594fea36ff7931ecc83bed89c04219289707e
Full Name <email@address.com>
2023-03-21 23:24:48 +0200

    Update dist

---
+++ _nuxt/Markdown.ed788e7c.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.2e39d4b2.js";import{f as o,q as f,h as u,k as m}from"./entry.d261a4dc.js";import"./utils.fc51fd43.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=f(),{between:n,default:a}=u(),r=m(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ _nuxt/ProseHr.46188a2a.js
@@ -0,0 +1,1 @@
+import{x as e,o as r,y as c}from"./entry.d261a4dc.js";const o={};function t(n,s){return r(),c("hr")}const _=e(o,[["render",t]]);export{_ as default};
