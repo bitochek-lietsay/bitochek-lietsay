a4f4f7fececce6e0adcdb3f30efe495350142328
Full Name <email@address.com>
2023-03-21 19:44:34 +0200

    Update dist

---
+++ _nuxt/ProseTable.a2854368.js
@@ -0,0 +1,1 @@
+import{x as o,o as r,y as t,v as s}from"./entry.c9a84a8c.js";const a={};function n(e,c){return r(),t("table",null,[s(e.$slots,"default")])}const f=o(a,[["render",n]]);export{f as default};
--- _nuxt/composables.9d004d3a.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./entry.bdefd17d.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
