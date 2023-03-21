12aab04e0f293eb1e6ff6553121de7708b710575
Full Name <email@address.com>
2023-03-21 23:35:21 +0200

    Update dist

---
+++ .output/public/_nuxt/ProseBlockquote.9ca20818.js
@@ -0,0 +1,1 @@
+import{x as o,o as t,y as r,v as c}from"./entry.cbe71771.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};
--- .output/public/_nuxt/composables.c1fd458d.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./entry.6ec99bf2.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
