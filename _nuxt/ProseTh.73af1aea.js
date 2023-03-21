c52e9d00c485265165c0479978859e31a1a67cc8
Full Name <email@address.com>
2023-03-21 22:56:44 +0200

    Update dist

---
+++ _nuxt/ProseTh.73af1aea.js
@@ -0,0 +1,1 @@
+import{x as o,o as r,y as t,v as s}from"./entry.3377c218.js";const n={};function c(e,a){return r(),t("th",null,[s(e.$slots,"default")])}const f=o(n,[["render",c]]);export{f as default};
--- _nuxt/composables.b4db223e.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./entry.c9a84a8c.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
