3c33c9553f34f1c03d60cfb18b9bbf0b0a9ad2cd
Full Name <email@address.com>
2023-03-21 23:30:51 +0200

    Update dist

--- .output/public/_nuxt/ProseHr.b7661262.js
+++
@@ -1,1 +0,0 @@
-import{x as e,o as r,y as c}from"./entry.bdf10dae.js";const o={};function t(n,s){return r(),c("hr")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ .output/public/_nuxt/ProseTr.35812cc5.js
@@ -0,0 +1,1 @@
+import{x as r,o,y as t,v as s}from"./entry.6ec99bf2.js";const n={};function c(e,a){return o(),t("tr",null,[s(e.$slots,"default")])}const f=r(n,[["render",c]]);export{f as default};
