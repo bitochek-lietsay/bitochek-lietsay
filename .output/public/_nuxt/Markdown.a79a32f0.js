bce938e701ab75fccc1de76bda8b6da475f4380b
Full Name <email@address.com>
2023-03-21 23:21:44 +0200

    Update dist

--- .output/public/_nuxt/Markdown.a79a32f0.js
+++
@@ -1,1 +0,0 @@
-import s from"./ContentSlot.7efec42e.js";import{f as o,q as f,h as u,k as m}from"./entry.3377c218.js";import"./utils.cdc0273d.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=f(),{between:n,default:a}=u(),r=m(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ .output/public/_nuxt/Markdown.aa83f4fa.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.5aabd4dc.js";import{f as o,q as f,h as u,k as m}from"./entry.039028c4.js";import"./utils.c8533b71.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=f(),{between:n,default:a}=u(),r=m(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
