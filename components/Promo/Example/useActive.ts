3c2f1bde83b756935187038a0018ce3c8d714b41
Full Name <email@address.com>
2023-03-21 18:37:39 +0200

    Update dist

--- components/Promo/Example/useActive.ts
+++
@@ -1,7 +0,0 @@
-export const useActive = createSharedComposable(() => {
-    const currentSlide = ref(0)
-
-    return {
-        currentSlide
-    }
-})
--- init-repo.sh
+++
@@ -1,1 +0,0 @@
-git worktree add .output/public index
