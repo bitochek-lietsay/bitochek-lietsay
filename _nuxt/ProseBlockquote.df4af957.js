dd3ae84f38b4e5a975026c53566d0b63c08ae3f9
Full Name <email@address.com>
2023-03-21 23:35:21 +0200

    Update dist

--- _nuxt/ProseBlockquote.df4af957.js
+++
@@ -1,1 +0,0 @@
-import{x as o,o as t,y as r,v as c}from"./entry.6ec99bf2.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};
